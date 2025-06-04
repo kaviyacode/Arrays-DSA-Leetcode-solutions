be2ab32207c25d7f2c275a7a04a0ee78af1f47ba
Kaviya Sri B <165787960+kaviyacode@users.noreply.github.com>
2025-06-04 20:56:34 +0530

    Time: 1427 ms (5.00%) | Memory: 53.8 MB (8.71%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i]) {
+                    count++;
+                }
+            }
+            if (count > n / 2) {
+                return nums[i];
+            }
+        }
+
+        return -1; // Majority element is guaranteed, so this won't execute
+    }
+}
